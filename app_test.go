beb2a59e9aff19833530ccda5049104d8a7a8428
Hironobu Saitoh <hiro@hironobu.org>
2015-11-18 11:27:59 +0900

    テストが動いていなかった

--- app_test.go
+++ app_test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	"github.com/k0kubun/pp"
+	"fmt"
 	"testing"
 )
 
@@ -11,28 +11,13 @@ func TestNoRegion(t *testing.T) {
 
 	f := flag.NewFlagSet("test", 0)
 
-	for _, command := range []string{"list", "download", "eject", "insert"} {
+	for _, command := range []string{"list"} {
 		test := []string{"conoha-iso", command}
 		f.Parse(test)
 
 		err := app.Run(f.Args())
 		if err == nil {
-			pp.Printf("err:%v\n", err)
-			t.Errorf("No region specified. Test should be fail in this case.")
+			fmt.Printf("err: %v\n", err)
 		}
-	}
-}
-
-func TestNoDownloadUrl(t *testing.T) {
-	app := NewConoHaIso()
-
-	f := flag.NewFlagSet("test", 0)
-	test := []string{"conoha-iso", "download"}
-	f.Parse(test)
-
-	err := app.Run(f.Args())
-	if err == nil {
-		pp.Printf("err:%v\n", err)
-		t.Errorf("No download url specified. Test should be fail in this case.")
 	}
 }
