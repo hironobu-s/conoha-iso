b7912aa440651a29beae91fb640449f0beb2ac78
Hironobu Saitoh <hiro@hironobu.org>
2016-02-12 10:23:28 +0000

    バージョンを更新

--- app.go
+++ app.go
@@ -28,7 +28,7 @@ func NewConoHaIso() *ConoHaIso {
 func (app *ConoHaIso) setup() {
 	app.Name = "conoha-iso"
 	app.Usage = "This app allow you to manage ISO images on ConoHa."
-	app.Version = "0.2.1" // Version should be updated by hand at each release.
+	app.Version = "0.2.2" // Version should be updated by hand at each release.
 
 	flags := []cli.Flag{
 		cli.StringFlag{
